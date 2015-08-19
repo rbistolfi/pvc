8dd11b0aa03e41d2a3375b23c66d924915acc416
Rodrigo Bistolfi <rbistolfi@gmail.com>
2015-08-19 00:54:53 -0300

    updated for latest eve

--- pvc/settings.py
+++ pvc/settings.py
@@ -9,7 +9,8 @@ SERVER_NAME = os.environ.get("hostname")
 URL_PREFIX = "api"
 API_VERSION = "1"
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
-ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+ITEM_METHODS = ['GET', 'PUT', 'PATCH', 'DELETE']
+IF_MATCH = False
 
 
 package_schema = {
@@ -28,77 +29,80 @@ package_schema = {
     'category': {
         'type': 'string',
         'minlength': 0,
-        'maxlength': 10,
+        'maxlength': 30,
     },
     'packages': {
         'type': 'list',
         'schema': {
-            'arch': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10,
-            },
-            'build': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10,
-            },
-            'repository': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10,
-            },
-            'required': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10000,
-            },
-            'conflicts': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10000,
-            },
-            'suggests': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10000,
-            },
-            'release': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10,
-            },
-            'version': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 10,
-            },
-            'filename': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 100,
-                'required': True,
-            },
-            'url': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 1000,
-            },
-            'md5': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 50,
-            },
-            'size compressed': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 100,
-            },
-            'size uncompressed': {
-                'type': 'string',
-                'minlength': 0,
-                'maxlength': 100,
-            },
+            'type': 'dict',
+            'schema': {
+                'arch': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 10,
+                },
+                'build': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 10,
+                },
+                'repository': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 100,
+                },
+                'required': {
+                    'type': 'list',
+                    'minlength': 0,
+                    'maxlength': 10000,
+                },
+                'conflicts': {
+                    'type': 'list',
+                    'minlength': 0,
+                    'maxlength': 10000,
+                },
+                'suggests': {
+                    'type': 'list',
+                    'minlength': 0,
+                    'maxlength': 10000,
+                },
+                'release': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 10,
+                },
+                'version': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 50,
+                },
+                'filename': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 100,
+                    'required': True,
+                },
+                'url': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 1000,
+                },
+                'md5': {
+                    'type': 'string',
+                    'minlength': 0,
+                    'maxlength': 50,
+                },
+                'compressed': {
+                    'type': 'float',
+                    'minlength': 0,
+                    'maxlength': 100,
+                },
+                'uncompressed': {
+                    'type': 'float',
+                    'minlength': 0,
+                    'maxlength': 100,
+                }
+            }
         }
     }
 }
@@ -114,21 +118,21 @@ package = {
     # additional read-only entry point. This way consumers can also perform
     # GET requests at '/package/<lastname>/'.
     'additional_lookup': {
-        'url': '[\w]+',
+        'url': 'string',
         'field': 'name'
     },
 
     # We choose to override global cache-control directives for this resource.
-    'cache_control': 'max-age=10,must-revalidate',
-    'cache_expires': 10,
+    #'cache_control': 'max-age=10,must-revalidate',
+    #'cache_expires': 10,
 
     # most global settings can be overridden at resource level
-    'resource_methods': ['GET', 'POST', 'DELETE'],
+    #'resource_methods': ['GET', 'POST', 'DELETE'],
 
     'schema': package_schema
 }
 
 
 DOMAIN = {
-    'package': package,
+    'package': package
 }
